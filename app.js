93b64bdce933c6de3407c7def9b619b78e7afe86
Saifullaht <mrsaifullah025@gmail.in>
2024-04-20 00:37:14 +0500

    Mini-Assigment

---
+++ app.js
@@ -0,0 +1,64 @@
+let matrix1 = [
+    [1, 5, 4],
+    [0, 5, 5],
+    [9, 5, 1],
+  ]; 
+  
+  let matrix2 = [
+    [1, 2, 4],
+    [5, 7, 5],
+    [5, 2, 1],
+  ];
+  
+ 
+ for (let i = 0; i < 3; i++) {
+for (let j = 0; j < 3; j++) {
+    
+    console.log(matrix1[i][j] + matrix2[i][j] + " " + " 1st ");
+}
+    
+    
+ } ;
+let matrix3 = [
+    [2, 3, 4],
+    [1, 2, 3],
+    [5, 2, 4],
+  ]; 
+  
+  let matrix4 = [
+    [8, 5, 1],
+    [5, 2, 5],
+    [2, 2, 1],
+  ];
+  
+ 
+ for (let i = 0; i < 3; i++) {
+for (let j = 0; j < 3; j++) {
+    
+    console.log(matrix3[i][j] + matrix4[i][j]  + " " + " 2st ");
+}
+    
+    
+ } ;
+let matrix5 = [
+    [4, 2, 3],
+    [5, 6, 2],
+    [5, 5, 2],
+  ]; 
+  
+  let matrix6 = [
+    [5, 3, 5],
+    [2, 8, 7],
+    [5, 4, 2],
+  ];
+  
+ 
+ for (let i = 0; i < 3; i++) {
+for (let j = 0; j < 3; j++) {
+    
+    console.log(matrix5[i][j] + matrix6[i][j] + " " + " 3rd ");
+}
+    
+    
+ } ;
+
